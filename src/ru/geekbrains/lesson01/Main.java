964d1423c675853e4098a2afe2dbce548eb323d8
Arseny Burov <arsenyburov@gmail.com>
2020-05-10 18:31:29 +0300

    first lesson

---
+++ src/ru/geekbrains/lesson01/Main.java
@@ -0,0 +1,63 @@
+package ru.geekbrains.lesson01;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        variables();
+        count(2.0f, 3.0f, 4.0f, 5.0f);
+        System.out.println(count(2.0f, 3.0f, 4.0f, 5.0f));
+        trueOrNot(4, 7);
+        System.out.println(trueOrNot(15, 7));
+        positiveOrNot(0);
+        isNegative(14);
+        System.out.println(isNegative(-14));
+        hello("Максим");
+    }
+
+    static void variables(){
+        byte myByte = 0;
+        short myShort = 30000;
+        int myInt = 2147483647;
+        long myLong = 9223372036854775807L;
+        float myFloat = 13.21f;
+        double myDouble = 123.321;
+        char myChar = 'c';
+        boolean myBoolean = true;
+    }
+
+    static float count(float a, float b, float c, float d){
+        return a * (b + (c / d));
+    }
+
+    static boolean trueOrNot(int a, int b){
+        if ((a + b) > 10 && (a + b) <= 20) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    static void positiveOrNot(int a){
+        if (a >= 0){
+            System.out.println("Введенное число положительное.");
+        }
+        else {
+            System.out.println("Введенное число отрицательное.");
+        }
+    }
+
+    static boolean isNegative(int a){
+        if (a < 0){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    static void hello(String name){
+        System.out.println("Привет, " + name + "!");
+    }
+}
